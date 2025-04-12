30eca7d67b342e5ac0cbc5a98c2282b9fa360b56
Paul Halpin <halpo@me.com>
2025-04-12 11:03:17 +0100

    speech update

--- speech.py
+++ speech.py
@@ -1,53 +1,32 @@
-import base64
-import os
-
-import httpx
+from google.cloud import texttospeech
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 from io import BytesIO
 from pydantic import BaseModel
 
 router = APIRouter()
-
-GOOGLE_TTS_URL = "https://texttospeech.googleapis.com/v1/text:synthesize"
-
-AUTH_TOKEN = os.environ.get("TTS_KEY")
-if not AUTH_TOKEN:
-    with open("speechKey.txt") as f:
-        AUTH_TOKEN = f.read().strip()
+client = texttospeech.TextToSpeechClient()
 
 class SpeakRequest(BaseModel):
     text: str
 
 @router.post("/speak")
 async def synthesize_speech(request: SpeakRequest):
-    headers = {
-        "Content-Type": "application/json",
-        "X-Goog-User-Project": "gov-sound",
-        "Authorization": f"Bearer {AUTH_TOKEN}"
-    }
+    synthesis_input = texttospeech.SynthesisInput({"text": request.text})
 
-    payload = {
-        "input": {
-            "text": request.text
-        },
-        "voice": {
-            "languageCode": "en-GB",
-            "name": "en-GB-Chirp3-HD-Puck"
-        },
-        "audioConfig": {
-            "audioEncoding": "LINEAR16"
-        }
-    }
+    voice = texttospeech.VoiceSelectionParams(
+        {"language_code": "en-US", "ssml_gender": texttospeech.SsmlVoiceGender.NEUTRAL}
+    )
 
-    async with httpx.AsyncClient() as client:
-        response = await client.post(GOOGLE_TTS_URL, headers=headers, json=payload)
+    audio_config = texttospeech.AudioConfig(
+        {"audio_encoding": texttospeech.AudioEncoding.MP3}
+    )
 
-    response.raise_for_status()
-    data = response.json()
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
 
-    audio_content = base64.b64decode(data["audioContent"])
-    audio_stream = BytesIO(audio_content)
+    audio_stream = BytesIO(response.audio_content)
 
     return StreamingResponse(audio_stream, media_type="audio/wav", headers={
         "Content-Disposition": "attachment; filename=output.wav"
