909c0dfe13503a6e50cf9b8e4ac7897f1555de0c
Paul <halpo@me.com>
2025-04-15 09:32:57 +0100

    eleven labs

--- speech.py
+++ speech.py
@@ -1,17 +1,42 @@
+from elevenlabs.core import ApiError
 from google.cloud import texttospeech
 from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
-from io import BytesIO
 from pydantic import BaseModel
+from io import BytesIO
+from elevenlabs.client import ElevenLabs
+
+with open("eleven.txt") as f:
+    key = f.read().strip()
+
+client = ElevenLabs(
+  api_key=key,
+)
 
 router = APIRouter()
-client = texttospeech.TextToSpeechClient()
 
 class SpeakRequest(BaseModel):
     text: str
 
 @router.post("/speak")
 async def synthesize_speech(request: SpeakRequest):
+    try:
+        audio_stream = client.text_to_speech.convert_as_stream(
+            text=request.text,
+            voice_id="zt0X5NTm9ZAYJ9qvmxSV",
+            model_id="eleven_multilingual_v2"
+        )
+        return StreamingResponse(audio_stream, media_type="audio/mp3", headers={
+            "Content-Disposition": "attachment; filename=output.mp3"
+        })
+    except ApiError:
+        return await fallback_speech(request)
+
+
+
+google_client = texttospeech.TextToSpeechClient()
+# @router.post("/speak")
+async def fallback_speech(request: SpeakRequest):
     synthesis_input = texttospeech.SynthesisInput({"text": request.text})
 
     voice = texttospeech.VoiceSelectionParams(
@@ -22,7 +47,7 @@ async def synthesize_speech(request: SpeakRequest):
         {"audio_encoding": texttospeech.AudioEncoding.MP3}
     )
 
-    response = client.synthesize_speech(
+    response = google_client.synthesize_speech(
         input=synthesis_input, voice=voice, audio_config=audio_config
     )
 
